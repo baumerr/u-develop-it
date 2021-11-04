c353104c73d5dff5e1078ca8235ad491702dd2d7
randy <baumer.randy@gmail.com>
2021-11-04 13:50:26 -0400

    added mysql start to server start

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mysql = require('mysql2');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -6,6 +7,19 @@ const app = express();
 // express middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+//connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        // Your MySQL username,
+        user: 'root',
+        // Your MySQL password
+        password: 'cul8erdude',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
 
 
 // Default response for any other request (NOT FOUND)
